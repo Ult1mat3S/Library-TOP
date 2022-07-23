a45c010e49fae0164debde217aac54826666a8a3
UItimate <52617311+UItimate@users.noreply.github.com>
2022-07-23 02:45:42 -0600

    fix: Replace all files

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let myLibrary = ['Fake Book: Edition 2', 'The Fake Book'];
+let myLibrary = [];
 let modal = document.getElementById("myModal");
 let span = document.getElementsByClassName("close")[0];
 
@@ -8,14 +8,40 @@ function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+    this.readStatus = read
     this.info = function () {
         return this.info;
     }
 }
 
 function addBookToLibrary() {
-    modal.style.display = "block";
+    getFormInput()
+
+    const container = document.querySelector('#container');
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('style', 'background-color: grey;');
+    container.appendChild(card);
+    const selectCard = document.querySelector('.card');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = `Title: ${title.value}`
+
+    const hr = document.createElement('hr');
+
+    const bookAuthor = document.createElement('p')
+    const bookPages = document.createElement('p')
+    const bookRead = document.createElement('p')
+
+    bookAuthor.textContent = `Book author: ${author.value}`
+    bookPages.textContent = `Book pages #: ${pages.value},`
+    bookRead.textContent = `Already read? ${readStatus.value}`
+
+    selectCard.appendChild(h1);
+    selectCard.appendChild(bookAuthor);
+    selectCard.appendChild(bookPages);
+    selectCard.appendChild(bookRead);
+    selectCard.appendChild(hr);
 }
 
 span.onclick = function () {
@@ -45,18 +71,18 @@ function printBook() {
         const selectCard = document.querySelector('.card');
 
         const h1 = document.createElement('h1');
-        h1.textContent = `Book ${i + 1}`
+        h1.textContent = `${book}`
 
         const hr = document.createElement('hr');
 
-        const h3 = document.createElement('h3')
-        h3.textContent = `${book}`
 
         const bookDetails = document.createElement('p')
-        bookDetails.textContent = "book author, book pages #, read/not read?"
+        bookDetails.textContent = `Book author:
+        Book pages #:
+        Read/not read?`
 
         selectCard.appendChild(h1);
-        selectCard.appendChild(h3);
+
         selectCard.appendChild(bookDetails);
         selectCard.appendChild(hr);
     }
@@ -69,54 +95,12 @@ function getFormInput() {
     console.log(title);
     let pages = document.querySelector('#pages').value;
     console.log(pages);
-    let read = document.querySelector('#read').value;
-    console.log(read);
-    return author;
-    // console.log()
-    // bookTitle: title,
-    // bookPages: pages,
-    // bookRead: read
-
-
+    let readStatus = document.querySelector('#readStatus').value;
+    console.log(readStatus);
 }
 
 function createNewBook() {
-    getFormInput()
-    console.log(author.value);
-    console.log(title.value);
-    console.log(pages.value);
-    console.log(read.value);
-    i = 0
-
-    const container = document.querySelector('#container');
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.setAttribute('style', 'background-color: grey;');
-    container.appendChild(card);
-    const selectCard = document.querySelector('.card');
-
-    const h1 = document.createElement('h1');
-    h1.textContent = `Book ${i + 1}`
-
-    const hr = document.createElement('hr');
-
-    const h3 = document.createElement('h3')
-    h3.textContent = `${title.value}`
-
-    const bookAuthor = document.createElement('p')
-    const bookPages = document.createElement('p')
-    const bookRead = document.createElement('p')
-
-    bookAuthor.textContent = `Book author: ${author.value}`
-    bookPages.textContent = `Book pages #: ${pages.value},`
-    bookRead.textContent = `Already read? ${read.value}`
-
-    selectCard.appendChild(h1);
-    selectCard.appendChild(h3);
-    selectCard.appendChild(bookAuthor);
-    selectCard.appendChild(bookPages);
-    selectCard.appendChild(bookRead);
-    selectCard.appendChild(hr);
+    modal.style.display = "block";
 
 }
 

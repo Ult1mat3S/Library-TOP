58c474e6091ad327ed6cd408f3e1d3d513875a83
UItimate <52617311+UItimate@users.noreply.github.com>
2022-07-28 02:36:44 -0600

    fix: replace missing curly brace

--- main.js
+++ main.js
@@ -59,56 +59,57 @@ function changeReadStatus() {
         readStatus.classList.remove('green')
         readStatus.classList.add('red')
     }
+}
 
 
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
+}
 
-    function printBook() {
-        for (let i = 0; i < myLibrary.length; i++) {
-            const book = myLibrary[i];
-            // console.log(myLibrary)
-            const container = document.querySelector('#container');
-            const card = document.createElement('div');
-            card.classList.add('card');
-            card.setAttribute('style', 'background-color: grey;');
-            container.appendChild(card);
-            const selectCard = document.querySelector('.card');
+function printBook() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = myLibrary[i];
+        // console.log(myLibrary)
+        const container = document.querySelector('#container');
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.setAttribute('style', 'background-color: grey;');
+        container.appendChild(card);
+        const selectCard = document.querySelector('.card');
 
-            const h1 = document.createElement('h1');
-            h1.textContent = `${book}`
+        const h1 = document.createElement('h1');
+        h1.textContent = `${book}`
 
-            const hr = document.createElement('hr');
+        const hr = document.createElement('hr');
 
 
-            const bookDetails = document.createElement('p')
-            bookDetails.textContent = `Book author:
+        const bookDetails = document.createElement('p')
+        bookDetails.textContent = `Book author:
         Book pages #:
         Read/not read?`
 
-            selectCard.appendChild(h1);
+        selectCard.appendChild(h1);
 
-            selectCard.appendChild(bookDetails);
-            selectCard.appendChild(hr);
-        }
+        selectCard.appendChild(bookDetails);
+        selectCard.appendChild(hr);
     }
+}
 
-    function getFormInput() {
-        let title = document.querySelector('#title').value;
-        console.log(title);
-        let author = document.querySelector('#author').value;
-        console.log(author);
-        let pages = document.querySelector('#pages').value;
-        console.log(pages);
-        let readStatus = document.querySelector('#readStatus').value;
-        console.log(readStatus);
-    }
+function getFormInput() {
+    let title = document.querySelector('#title').value;
+    console.log(title);
+    let author = document.querySelector('#author').value;
+    console.log(author);
+    let pages = document.querySelector('#pages').value;
+    console.log(pages);
+    let readStatus = document.querySelector('#readStatus').value;
+    console.log(readStatus);
+}
 
-    function createNewBook() {
-        modal.style.display = "block";
-    }
+function createNewBook() {
+    modal.style.display = "block";
+}
 
-    printBook()
+printBook()
